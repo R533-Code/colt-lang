67a8a19cb1f4a147157e160e2cadd41ac8b38ea9
R533-Code <raphaeldibnehmepc@gmail.com>
2022-05-26 17:17:21 +0300

    Transformed byte_code to c++

---
+++ colti/src/byte-code/byte_code.cpp
@@ -0,0 +1,128 @@
+/** @file byte_code.c
+* Contains the definitions of the functions declared in 'byte_code.h'
+*/
+
+#include "byte_code.h"
+
+namespace colti::byte_code
+{
+	QWORD OpCode_Negate(QWORD value, OperandType type)
+	{
+		QWORD result;
+		switch (type)
+		{
+		break; case OperandType::Int8:		result.i8 = -value.i8;
+		break; case OperandType::Int16:		result.i16 = -value.i16;
+		break; case OperandType::Int32:		result.i32 = -value.i32;
+		break; case OperandType::Int64:		result.i64 = -value.i64;
+		break; case OperandType::Float:		result.f = -value.f;
+		break; case OperandType::Double:	result.d = -value.d;
+		break; default: colti_assert(false, "Invalid operand for OP_ADD!");
+		}
+		return result;
+	}
+
+	QWORD OpCode_Convert(QWORD value, OperandType from, OperandType to)
+	{
+		colti_assert(false, "Still not implemented!");
+	}
+
+	QWORD OpCode_Sum(QWORD left, QWORD right, OperandType type)
+	{
+		QWORD result;
+		switch (type)
+		{
+		break; case OperandType::Int8:		result.i8 = left.i8 + right.i8;
+		break; case OperandType::Int16:		result.i16 = left.i16 + right.i16;
+		break; case OperandType::Int32:		result.i32 = left.i32 + right.i32;
+		break; case OperandType::Int64:		result.i64 = left.i64 + right.i64;
+		break; case OperandType::UInt8:		result.ui8 = left.ui8 + right.ui8;
+		break; case OperandType::UInt16:	result.ui16 = left.ui16 + right.ui16;
+		break; case OperandType::UInt32:	result.ui32 = left.ui32 + right.ui32;
+		break; case OperandType::UInt64:	result.ui64 = left.ui64 + right.ui64;
+		break; case OperandType::Float:		result.f = left.f + right.f;
+		break; case OperandType::Double:	result.d = left.d + right.d;
+		break; default: colti_assert(false, "Invalid operand for OP_ADD!");
+		}
+		return result;
+	}
+
+	QWORD OpCode_Difference(QWORD left, QWORD right, OperandType type)
+	{
+		QWORD result;
+		switch (type)
+		{
+		break; case OperandType::Int8:		result.i8 = left.i8 - right.i8;
+		break; case OperandType::Int16:		result.i16 = left.i16 - right.i16;
+		break; case OperandType::Int32:		result.i32 = left.i32 - right.i32;
+		break; case OperandType::Int64:		result.i64 = left.i64 - right.i64;
+		break; case OperandType::UInt8:		result.ui8 = left.ui8 - right.ui8;
+		break; case OperandType::UInt16:		result.ui16 = left.ui16 - right.ui16;
+		break; case OperandType::UInt32:		result.ui32 = left.ui32 - right.ui32;
+		break; case OperandType::UInt64:		result.ui64 = left.ui64 - right.ui64;
+		break; case OperandType::Float:	result.f = left.f - right.f;
+		break; case OperandType::Double:	result.d = left.d - right.d;
+		break; default: colti_assert(false, "Invalid operand for OP_ADD!");
+		}
+		return result;
+	}
+
+	QWORD OpCode_Multiply(QWORD left, QWORD right, OperandType type)
+	{
+		QWORD result;
+		switch (type)
+		{
+		break; case OperandType::Int8:		result.i8 = left.i8 * right.i8;
+		break; case OperandType::Int16:		result.i16 = left.i16 * right.i16;
+		break; case OperandType::Int32:		result.i32 = left.i32 * right.i32;
+		break; case OperandType::Int64:		result.i64 = left.i64 * right.i64;
+		break; case OperandType::UInt8:		result.ui8 = left.ui8 * right.ui8;
+		break; case OperandType::UInt16:		result.ui16 = left.ui16 * right.ui16;
+		break; case OperandType::UInt32:		result.ui32 = left.ui32 * right.ui32;
+		break; case OperandType::UInt64:		result.ui64 = left.ui64 * right.ui64;
+		break; case OperandType::Float:	result.f = left.f * right.f;
+		break; case OperandType::Double:	result.d = left.d * right.d;
+		break; default: colti_assert(false, "Invalid operand for OP_ADD!");
+		}
+		return result;
+	}
+
+	QWORD OpCode_Divide(QWORD left, QWORD right, OperandType type)
+	{
+		QWORD result;
+		switch (type)
+		{
+		break; case OperandType::Int8:		result.i8 = left.i8 / right.i8;
+		break; case OperandType::Int16:		result.i16 = left.i16 / right.i16;
+		break; case OperandType::Int32:		result.i32 = left.i32 / right.i32;
+		break; case OperandType::Int64:		result.i64 = left.i64 / right.i64;
+		break; case OperandType::UInt8:		result.ui8 = left.ui8 / right.ui8;
+		break; case OperandType::UInt16:		result.ui16 = left.ui16 / right.ui16;
+		break; case OperandType::UInt32:		result.ui32 = left.ui32 / right.ui32;
+		break; case OperandType::UInt64:		result.ui64 = left.ui64 / right.ui64;
+		break; case OperandType::Float:	result.f = left.f / right.f;
+		break; case OperandType::Double:	result.d = left.d / right.d;
+		break; default: colti_assert(false, "Invalid operand for OP_ADD!");
+		}
+		return result;
+	}
+
+	void OpCode_Print(QWORD value, OperandType type)
+	{
+		switch (type)
+		{
+		break; case OperandType::Bool:		printf("%s", value.b ? "true\n" : "false\n");
+		break; case OperandType::Int8:		printf("%"PRId8"\n", value.ui8);
+		break; case OperandType::Int16:		printf("%"PRId16"\n", value.ui16);
+		break; case OperandType::Int32:		printf("%"PRId32"\n", value.ui32);
+		break; case OperandType::Int64:		printf("%"PRId64"\n", value.ui64);
+		break; case OperandType::UInt8:		printf("%"PRIu8"\n", value.ui8);
+		break; case OperandType::UInt16:	printf("%"PRIu16"\n", value.ui16);
+		break; case OperandType::UInt32:	printf("%"PRIu32"\n", value.ui32);
+		break; case OperandType::UInt64:	printf("%"PRIu64"\n", value.ui64);
+		break; case OperandType::Float:		printf("%g\n", value.f);
+		break; case OperandType::Double:	printf("%g\n", value.d);
+		break; default: colti_assert(false, "Invalid operand for OP_PRINT!");
+		}
+	}
+}
--- colti/src/byte-code/byte_code.h
+++ colti/src/byte-code/byte_code.h
@@ -15,122 +15,127 @@
 
 #include "common.h"
 
-/// @brief Represents an instruction to be executed by the VM
-typedef enum
+namespace colti::byte_code
 {
-	//IMMEDIATE VALUES LOADING
-	/// @brief Specifies that the next byte-code is an immediate byte
-	OP_IMMEDIATE_BYTE,
-	/// @brief Specifies that a 2 bytes (aligned) WORD is written in the following byte-codes
-	OP_IMMEDIATE_WORD,
-	/// @brief Specifies that a 4 bytes (aligned) DWORD is written in the following byte-codes
-	OP_IMMEDIATE_DWORD,
-	/// @brief Specifies that an 8 bytes (aligned) QWORD is written in the following byte-codes
-	OP_IMMEDIATE_QWORD,
+	/// @brief Represents an instruction to be executed by the VM
+	enum class OpCode
+		: uint8_t
+	{
+		//IMMEDIATE VALUES LOADING
+		/// @brief Specifies that the next byte-code is an immediate byte
+		OP_IMMEDIATE_BYTE,
+		/// @brief Specifies that a 2 bytes (aligned) WORD is written in the following byte-codes
+		OP_IMMEDIATE_WORD,
+		/// @brief Specifies that a 4 bytes (aligned) DWORD is written in the following byte-codes
+		OP_IMMEDIATE_DWORD,
+		/// @brief Specifies that an 8 bytes (aligned) QWORD is written in the following byte-codes
+		OP_IMMEDIATE_QWORD,
 
-	/// @brief Specifies that the next byte is an operand to which to cast a QWORD before negating its sign
-	OP_NEGATE,
+		/// @brief Specifies that the next byte is an operand to which to cast a QWORD before negating its sign
+		OP_NEGATE,
 
-	/// @brief Specifies that the next 2 bytes are operands type, the first to which to cast a QWORD before converting it to the second
-	OP_CONVERT,
+		/// @brief Specifies that the next 2 bytes are operands type, the first to which to cast a QWORD before converting it to the second
+		OP_CONVERT,
 
-	/// @brief Specifies that the next byte is an operand to which to cast 2 QWORD before doing their sum
-	OP_ADD,
-	/// @brief Specifies that the next byte is an operand to which to cast 2 QWORD before doing their difference
-	OP_SUBTRACT,
-	/// @brief Specifies that the next byte is an operand to which to cast 2 QWORD before doing their product
-	OP_MULTIPLY,
-	/// @brief Specifies that the next byte is an operand to which to cast 2 QWORD before doing their division
-	OP_DIVIDE,
+		/// @brief Specifies that the next byte is an operand to which to cast 2 QWORD before doing their sum
+		OP_ADD,
+		/// @brief Specifies that the next byte is an operand to which to cast 2 QWORD before doing their difference
+		OP_SUBTRACT,
+		/// @brief Specifies that the next byte is an operand to which to cast 2 QWORD before doing their product
+		OP_MULTIPLY,
+		/// @brief Specifies that the next byte is an operand to which to cast 2 QWORD before doing their division
+		OP_DIVIDE,
 
-	//DON'T KNOW
-	OP_MODULO,
+		//DON'T KNOW
+		OP_MODULO,
 
-	/// @brief Specifies that the next byte is an operand to which to cast a QWORD before printing it (for debug purposes)
-	OP_PRINT,
+		/// @brief Specifies that the next byte is an operand to which to cast a QWORD before printing it (for debug purposes)
+		OP_PRINT,
 
-	//MISCALLENEOUS
-	OP_RETURN,
-} OpCode;
+		//MISCALLENEOUS
+		OP_RETURN,
+	};
 
 
-/// @brief Represents an operand type
-typedef enum
-{
-	/// @brief Represents a bool
-	COLTI_BOOL		= OPERAND_COLTI_BOOL,
-	/// @brief Represents a 64-bit double-precision float
-	COLTI_DOUBLE	= OPERAND_COLTI_DOUBLE,
-	/// @brief Represents a 32-bit single-precision float
-	COLTI_FLOAT		= OPERAND_COLTI_FLOAT,
+	/// @brief Represents an operand type
+	enum class OperandType
+		: uint8_t
+	{
+		/// @brief Represents a bool
+		Bool	= OPERAND_COLTI_BOOL,
+		/// @brief Represents a 64-bit double-precision float
+		Double	= OPERAND_COLTI_DOUBLE,
+		/// @brief Represents a 32-bit single-precision float
+		Float	= OPERAND_COLTI_FLOAT,
 
-	/// @brief Represents a signed 8-bit integer
-	COLTI_INT8		= OPERAND_COLTI_I8,
-	/// @brief Represents a signed 16-bit integer
-	COLTI_INT16		= OPERAND_COLTI_I16,
-	/// @brief Represents a signed 32-bit integer
-	COLTI_INT32		= OPERAND_COLTI_I32,
-	/// @brief Represents a signed 64-bit integer
-	COLTI_INT64		= OPERAND_COLTI_I64,
+		/// @brief Represents a signed 8-bit integer
+		Int8	= OPERAND_COLTI_I8,
+		/// @brief Represents a signed 16-bit integer
+		Int16	= OPERAND_COLTI_I16,
+		/// @brief Represents a signed 32-bit integer
+		Int32	= OPERAND_COLTI_I32,
+		/// @brief Represents a signed 64-bit integer
+		Int64	= OPERAND_COLTI_I64,
 
-	/// @brief Represents an unsigned 8-bit integer
-	COLTI_UINT8		= OPERAND_COLTI_UI8,
-	/// @brief Represents an unsigned 16-bit integer
-	COLTI_UINT16	= OPERAND_COLTI_UI16,
-	/// @brief Represents an unsigned 32-bit integer
-	COLTI_UINT32	= OPERAND_COLTI_UI32,
-	/// @brief Represents an unsigned 64-bit integer
-	COLTI_UINT64	= OPERAND_COLTI_UI64,
-} OperandType;
+		/// @brief Represents an unsigned 8-bit integer
+		UInt8	= OPERAND_COLTI_UI8,
+		/// @brief Represents an unsigned 16-bit integer
+		UInt16	= OPERAND_COLTI_UI16,
+		/// @brief Represents an unsigned 32-bit integer
+		UInt32	= OPERAND_COLTI_UI32,
+		/// @brief Represents an unsigned 64-bit integer
+		UInt64	= OPERAND_COLTI_UI64,
+	};
 
-/**********************************
-BYTE-CODE RUNNING
-**********************************/
+	/**********************************
+	BYTE-CODE RUNNING
+	**********************************/
 
-/// @brief Casts 'value' to 'type', negates the result and return it
-/// @param value The QWORD on which to operate
-/// @param type The type to which to cast the QWORD
-/// @return The modified QWORD
-QWORD OpCode_Negate(QWORD value, OperandType type);
+	/// @brief Casts 'value' to 'type', negates the result and return it
+	/// @param value The QWORD on which to operate
+	/// @param type The type to which to cast the QWORD
+	/// @return The modified QWORD
+	QWORD OpCode_Negate(QWORD value, OperandType type);
 
-/// @brief Casts 'value' from 'from' to 'to'
-/// @param value The QWORD on which to operate
-/// @param from The type of the QWORD
-/// @param to The type to which to cast the QWORD
-/// @return The modified QWORD
-QWORD OpCode_Convert(QWORD value, OperandType from, OperandType to);
+	/// @brief Casts 'value' from 'from' to 'to'
+	/// @param value The QWORD on which to operate
+	/// @param from The type of the QWORD
+	/// @param to The type to which to cast the QWORD
+	/// @return The modified QWORD
+	QWORD OpCode_Convert(QWORD value, OperandType from, OperandType to);
 
-/// @brief Casts 2 QWORD and return their sums
-/// @param left The left hand side
-/// @param right The right hand side
-/// @param type The type of the QWORDs
-/// @return The sum of the QWORDs
-QWORD OpCode_Sum(QWORD left, QWORD right, OperandType type);
+	/// @brief Casts 2 QWORD and return their sums
+	/// @param left The left hand side
+	/// @param right The right hand side
+	/// @param type The type of the QWORDs
+	/// @return The sum of the QWORDs
+	QWORD OpCode_Sum(QWORD left, QWORD right, OperandType type);
 
-/// @brief Casts 2 QWORD and return their difference
-/// @param left The left hand side
-/// @param right The right hand side
-/// @param type The type of the QWORDs
-/// @return The difference of the QWORDs
-QWORD OpCode_Difference(QWORD left, QWORD right, OperandType type);
+	/// @brief Casts 2 QWORD and return their difference
+	/// @param left The left hand side
+	/// @param right The right hand side
+	/// @param type The type of the QWORDs
+	/// @return The difference of the QWORDs
+	QWORD OpCode_Difference(QWORD left, QWORD right, OperandType type);
 
-/// @brief Casts 2 QWORD and return their multiplication
-/// @param left The left hand side
-/// @param right The right hand side
-/// @param type The type of the QWORDs
-/// @return The multiplication of the QWORDs
-QWORD OpCode_Multiply(QWORD left, QWORD right, OperandType type);
+	/// @brief Casts 2 QWORD and return their multiplication
+	/// @param left The left hand side
+	/// @param right The right hand side
+	/// @param type The type of the QWORDs
+	/// @return The multiplication of the QWORDs
+	QWORD OpCode_Multiply(QWORD left, QWORD right, OperandType type);
 
-/// @brief Casts 2 QWORD and return their division
-/// @param left The left hand side
-/// @param right The right hand side
-/// @param type The type of the QWORDs
-/// @return The division of the QWORDs
-QWORD OpCode_Divide(QWORD left, QWORD right, OperandType type);
+	/// @brief Casts 2 QWORD and return their division
+	/// @param left The left hand side
+	/// @param right The right hand side
+	/// @param type The type of the QWORDs
+	/// @return The division of the QWORDs
+	QWORD OpCode_Divide(QWORD left, QWORD right, OperandType type);
 
-/// @brief Casts 'value' to 'type' then prints its value, for DEBUG purposes
-/// @param value The QWORD to print
-/// @param type The type of the QWORD
-void OpCode_Print(QWORD value, OperandType type);
+	/// @brief Casts 'value' to 'type' then prints its value, for DEBUG purposes
+	/// @param value The QWORD to print
+	/// @param type The type of the QWORD
+	void OpCode_Print(QWORD value, OperandType type);
+}
 
 #endif //HG_COLTI_BYTE_CODE
