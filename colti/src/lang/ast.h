bcb48eb0921ae6861098966e20619fe9acac4fdb
R533-Code <raphaeldibnehmepc@gmail.com>
2022-05-27 16:11:38 +0300

    Added some declarations

--- colti/src/lang/ast.h
+++ colti/src/lang/ast.h
@@ -2,7 +2,26 @@
 #define HG_COLTI_AST
 
 #include "common.h"
+#include "token.h"
+#include "scanner.h"
+#include "expr.h"
 
+typedef struct
+{
+	/// @brief The AST
+	Expr* expr;
+	/// @brief The scanner providing the lexemes for the AST
+	Scanner scan;
+} AST;
 
+void ASTInit(AST* ast, StringView to_parse);
+
+void ASTFree(AST* ast);
+
+/************************************
+IMPLEMENTATION HELPERS
+************************************/
+
+void impl_expr_free(Expr* expr);
 
 #endif //HG_COLTI_AST
