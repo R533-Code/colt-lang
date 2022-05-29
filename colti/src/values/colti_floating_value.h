4a4bdcff207ea7a7e23077f1c56a006a44b7a2a8
R533-Code <raphaeldibnehmepc@gmail.com>
2022-05-29 13:47:53 +0300

    Fixed string constant with should be array not pointer

--- colti/src/values/colti_floating_value.h
+++ colti/src/values/colti_floating_value.h
@@ -10,13 +10,13 @@ typedef float ColtiFloat;
 /// @brief The type-id of a Colt float
 #define COLTI_FLOAT_ID 9
 /// @brief A string representing a Colt single-precision float
-static const char* const ColtFloat_str = "float";
+static const char ColtFloat_str[] = "float";
 
 /// @brief Colti Double (8 bytes double precision float)
 typedef double ColtiDouble;
 /// @brief The type-id of a Colt double
 #define COLTI_DOUBLE_ID 10
 /// @brief A string representing a Colt double-precision float
-static const char* const ColtDouble_str = "double";
+static const char ColtDouble_str[] = "double";
 
 #endif //HG_COLTI_FLOATING_VALUE
