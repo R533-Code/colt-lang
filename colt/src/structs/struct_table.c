28ec24ee0c68d9d0fe0c19784e7edd6a1f729f92
R533-Code <raphaeldibnehmepc@gmail.com>
2022-06-03 15:05:23 +0300

    Added Table with most of its methods

---
+++ colt/src/structs/struct_table.c
@@ -0,0 +1,114 @@
+/** @file parse_args.c
+* Contains the definitions of the functions declared in 'parse_args.h'
+*/
+
+#include "struct_table.h"
+
+void TableInit(Table* table)
+{
+	table->capacity = 10;
+	table->count = 0;
+	table->entries = safe_malloc(sizeof(Entry) * 10);
+	for (size_t i = 0; i < table->capacity; i++)
+		table->entries->key.start = NULL;
+}
+
+void TableFree(Table* table)
+{
+	safe_free(table->entries);
+}
+
+bool TableGet(Table* table, StringView key, QWORD* value)
+{
+	if (table->count == 0)
+		return false;
+
+	const Entry* entry = table_find_entry(table->entries, table->capacity, key);
+	if (entry->key.start == NULL)
+		return false;
+
+	*value = entry->value;
+	return true;
+}
+
+bool TableSet(Table* table, StringView strv, QWORD value, Type type)
+{
+	if ((double)table->count + 1 > (double)table->capacity * TABLE_MAX_LOAD)
+	{
+		table_double_capacity(table);
+	}
+
+	Entry* entry = table_find_entry(table, strv);
+	bool isNewKey = (entry->key.end == entry->key.start);
+	if (isNewKey)
+		table->count++;
+
+	entry->key = strv;
+	entry->value = value;
+	entry->type = type;
+	return isNewKey;
+}
+
+bool TableDelete(Table* table, StringView key)
+{
+	if (table->count == 0) return false;
+
+	// Find the entry.
+	Entry* entry = table_find_entry(table->entries, table->capacity, key);
+	if (entry->key.start == NULL)
+		return false;
+
+	entry->key.start = NULL;
+	DO_IF_DEBUG_BUILD(entry->key.end = NULL);
+	return true;
+}
+
+uint64_t hash_strv(StringView strv)
+{
+	//FNV-1a hashing method
+	//https://en.wikipedia.org/wiki/Fowler%E2%80%93Noll%E2%80%93Vo_hash_function#FNV-1a_hash
+
+	//FNV_offset_basis
+	uint64_t hash = 0xcbf29ce484222325;
+	for (size_t i = 0; i < strv.end - strv.start; i++)
+	{
+		hash ^= (uint8_t)strv.start[i];
+		hash *= 0x100000001b3; //FNV_prime
+	}
+	return hash;
+}
+
+void table_grow_capacity(Table* table, uint64_t capacity)
+{
+	Entry* entries = safe_malloc(sizeof(Entry) * capacity);
+	for (size_t i = 0; i < capacity; i++)
+		entries->key.start = NULL;
+
+	for (size_t i = 0; i < table->capacity; i++)
+	{
+		const Entry* entry = &table->entries[i];
+		if (entry->key.start == NULL)
+			continue;
+
+		Entry* dest = table_find_entry(entries, capacity, entry->key);
+		dest->key = entry->key;
+		dest->value = entry->value;
+	}
+
+	safe_free(table->entries);
+	table->entries = entries;
+	table->capacity = capacity;
+}
+
+Entry* table_find_entry(Entry* entries, uint64_t capacity, StringView strv)
+{
+	uint64_t index = hash_strv(strv) % capacity;
+	for (;;)
+	{
+		Entry* entry = &entries[index];
+		if (StringViewEqual(entry->key, strv) || entry->key.start == NULL)
+			return entry;
+
+		index = (index + 1) % capacity;
+	}
+}
---
+++ colt/src/structs/struct_table.h
@@ -0,0 +1,55 @@
+#ifndef HG_COLT_STRUCT_TABLE
+#define HG_COLT_STRUCT_TABLE
+
+#include "common.h"
+#include "struct_string.h"
+
+/// @brief The maximum occupied capacity before growing the table
+#define TABLE_MAX_LOAD 0.75
+
+typedef struct
+{
+	StringView key;
+	QWORD value;
+	Type type;
+} Entry;
+
+typedef struct
+{
+	uint64_t count;
+	uint64_t capacity;
+	Entry* entries;
+} Table;
+
+/// @brief Initializes a Table
+/// @param table The Table to initialize
+void TableInit(Table* table);
+
+/// @brief Frees the resources used by a Table
+/// @param table The Table whose resources to free
+void TableFree(Table* table);
+
+bool TableGet(Table* table, StringView key, QWORD* value);
+
+bool TableSet(Table* table, StringView strv, QWORD value, Type type);
+
+bool TableDelete(Table* table, StringView key);
+
+/**************************************
+IMPLEMENTATION HELPERS
+**************************************/
+
+/// @brief Hashes a StringView.
+/// Uses FNV-1a as a hash function.
+/// @param strv The StringView to hash
+/// @return The unsigned int representing the hash
+uint64_t hash_strv(StringView strv);
+
+/// @brief Doubles the capacity of the table
+/// @param table The Table to modify
+/// @param capacity
+void table_grow_capacity(Table* table, uint64_t capacity);
+
+Entry* table_find_entry(Entry* entries, uint64_t capacity, StringView strv);
+
+#endif //HG_COLT_STRUCT_TABLE
