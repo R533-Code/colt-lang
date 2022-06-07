5a2c2635383a0d9baa4a9b4a3e2478938fbd0187
R533-Code <raphaeldibnehmepc@gmail.com>
2022-06-07 21:32:18 +0300

    Fixed comments

--- colt/src/type/colti_string_value.h
+++ colt/src/type/colti_string_value.h
@@ -1,15 +1,15 @@
 /** @file colti_integer_value.h
-* Contains typedefs representing integers and bool values in Colt.
+* Contains typedefs representing string values in Colt.
 */
 
 #ifndef HG_COLT_STRING_VALUE
 #define HG_COLT_STRING_VALUE
 
 /// @brief Pointer to a constant
-typedef const char* ColtLiteralString_t;
-/// @brief The type-id of a Colt bool
+typedef const char* ColtLString_t;
+/// @brief The type-id of a Colt literal string
 #define COLTI_LSTRING_ID 11
-/// @brief A string representing a Colt bool
-static const char ColtLiteralString_str[] = "l-string";
+/// @brief A string representing a Colt literal string
+static const char ColtLString_str[] = "lstring";
 
 #endif //HG_COLT_INTEGER_VALUE
