6ac99b5bc9b8bb65269900235a20caf8b720077f
R533-Code <raphaeldibnehmepc@gmail.com>
2022-06-07 20:52:36 +0300

    Added colti_string_value

---
+++ colt/src/values/colti_string_value.h
@@ -0,0 +1,15 @@
+/** @file colti_integer_value.h
+* Contains typedefs representing integers and bool values in Colt.
+*/
+
+#ifndef HG_COLT_STRING_VALUE
+#define HG_COLT_STRING_VALUE
+
+/// @brief Pointer to a constant
+typedef const char* ColtLiteralString_t;
+/// @brief The type-id of a Colt bool
+#define COLTI_CSTRING_ID 11
+/// @brief A string representing a Colt bool
+static const char ColtLiteralString_str[] = "l-string";
+
+#endif //HG_COLT_INTEGER_VALUE
