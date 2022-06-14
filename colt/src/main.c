2a26b008b3baa8e7b58d567274f5adc8c750026c
R533-Code <raphaeldibnehmepc@gmail.com>
2022-06-14 08:47:38 +0300

    Added parsing and running of file

--- colt/src/main.c
+++ colt/src/main.c
@@ -1,6 +1,29 @@
 #include "precomph.h"
 #include "vm/repl.h"
 
+void ColtRun(const char* byte_code_out, StringView to_parse)
+{
+	AST ast;
+	ASTInit(&ast);
+	if (ASTParse(&ast, to_parse))
+	{
+		Chunk chunk;
+		ChunkInit(&chunk);
+		if (generateByteCode(&chunk, &ast.table, &ast.expr))
+		{
+			if (byte_code_out != NULL)
+				ChunkSerialize(&chunk, byte_code_out);
+			StackVM vm;
+			StackVMInit(&vm);
+			StackVMRun(&vm, &chunk);
+			fputc('\n', stdout);
+			StackVMFree(&vm);
+		}
+		ChunkFree(&chunk);
+	}
+	ASTFree(&ast);
+}
+
 int main(int argc, const char** argv)
 {
 	ParseResult args = ParseArguments(argc, argv);
@@ -11,7 +34,8 @@ int main(int argc, const char** argv)
 	else
 	{
 		String file_content = StringGetFileContent(args.file_path_in);
-		//PARSE
+		//Parses and run if valid.
+		ColtRun(args.byte_code_out, StringToStringViewWithNUL(&file_content));		
 		StringFree(&file_content);
 	}
 	DUMP_MEMORY_LEAKS();
