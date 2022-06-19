e32fd8fa7d727468f6f45f203ca7a90831b037b3
R533-Code <raphaeldibnehmepc@gmail.com>
2022-06-19 19:11:44 +0300

    Added option header which contains options for scanning

---
+++ colt/src/lang/option.h
@@ -0,0 +1,21 @@
+#ifndef HG_COLT_OPTION
+#define HG_COLT_OPTION
+
+#include <stdbool.h>
+
+typedef struct
+{
+	/// @brief If false, warns if an expression result is not used.
+	/// When REPLing should be true: we don't want to warn if the user is doing arithmetic
+	bool no_warn_unused_result;
+	/// @brief If false, optimizes constant expressions
+	bool no_constant_folding;
+	/// @brief If false, allows messages to be printed
+	bool no_message;
+	/// @brief If false, allows warnings to be printed
+	bool no_warning;
+	/// @brief If false, allows errors to be printed
+	bool no_error;
+} ColtScanOptions;
+
+#endif //HG_COLT_OPTION
