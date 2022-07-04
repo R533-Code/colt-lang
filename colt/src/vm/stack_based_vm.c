69c534b3e71ce33ee2f25fdbec0ddb84bb399932
R533-Code <raphaeldibnehmepc@gmail.com>
2022-07-04 20:37:43 +0300

    Replace ' ' -> '\0' for singular when printing Stack QWORDs

--- colt/src/vm/stack_based_vm.c
+++ colt/src/vm/stack_based_vm.c
@@ -421,7 +421,7 @@ int64_t StackVMRun(StackVM* vm, Chunk* chunk)
 		break; case OP_EXIT:
 		{
 			uint64_t size = StackVMSize(vm);
-			printf("\nStack contains %"PRIu64" QWORD%c.", size, size == 1 ? ' ' : 's');
+			printf("\nStack contains %"PRIu64" QWORD%c.", size, size == 1 ? '\0' : 's');
 			return unsafe_get_qword(&ip).i64;
 		}
 		break; default:
