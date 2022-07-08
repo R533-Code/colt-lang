4ff6290eb21f3fd85bb90d2b46fbc2971fa362f2
R533-Code <raphaeldibnehmepc@gmail.com>
2022-07-08 19:16:00 +0300

    Remove const SECTION related functionality and renamed functions

--- colt/src/byte-code/disassemble.c
+++ colt/src/byte-code/disassemble.c
@@ -24,8 +24,6 @@ void ChunkDisassemble(const Chunk* chunk, const char* name)
 
 	//GLOBAL OFFSET
 	uint64_t global_offset = ChunkGetGLOBALSection(chunk);
-	//CONST OFFSET
-	uint64_t const_offset = ChunkGetCONSTSection(chunk);
 	//STRING OFFSET
 	uint64_t string_offset = ChunkGetSTRINGSection(chunk);
 	//DEBUG OFFSET
@@ -36,8 +34,8 @@ void ChunkDisassemble(const Chunk* chunk, const char* name)
 	fputs(CONSOLE_COLOR_REVERSE"SECTION HEADER:\n"CONSOLE_COLOR_RESET, stdout);
 	fputs("        ABI: ", stdout);
 	ChunkPrintABI(chunk, stdout);
-	printf("\n        %08"PRIu64": SECTION GLOBAL\n        %08"PRIu64": SECTION CONST\n        %08"PRIu64": SECTION STRING\n        %08"PRIu64": SECTION DEBUG\n        %08"PRIu64": SECTION CODE\n",
-		global_offset, const_offset, string_offset, debug_offset, code_offset
+	printf("\n        %08"PRIu64": SECTION GLOBAL\n        %08"PRIu64": SECTION STRING\n        %08"PRIu64": SECTION DEBUG\n        %08"PRIu64": SECTION CODE\n",
+		global_offset, string_offset, debug_offset, code_offset
 	);
 
 	if (global_offset != 0)
@@ -48,27 +46,11 @@ void ChunkDisassemble(const Chunk* chunk, const char* name)
 		if (debug_offset != 0)
 		{
 			for (size_t i = 0; i < (global_end - global_offset) / sizeof(QWORD); i++)
-				impl_print_global_variable(chunk, i);
+				unsafe_print_global_variable(chunk, i);
 		}
 		else
 		{
 			for (size_t i = global_offset; i < global_end; i += sizeof(QWORD))
-				printf("        %08"PRIu64": 0x%"PRIX64, i, *(uint64_t*)chunk->code + i);
-		}
-	}
-	if (const_offset != 0)
-	{
-		fputs(CONSOLE_COLOR_REVERSE"SECTION CONST:\n"CONSOLE_COLOR_RESET, stdout);
-		const uint64_t const_end = unsafe_chunk_get_const_end(chunk);
-		//if there are debug symbols
-		if (debug_offset != 0)
-		{
-			for (size_t i = 0; i < const_end; i++)
-				impl_print_global_variable(chunk, i);
-		}
-		else
-		{
-			for (size_t i = const_offset; i < const_end; i += sizeof(QWORD))
 				printf("        %08"PRIu64": 0x%"PRIX64, i, *(uint64_t*)chunk->code + i);
 		}
 	}
@@ -84,7 +66,7 @@ void ChunkDisassemble(const Chunk* chunk, const char* name)
 		for (size_t i = 0; i < string_literal_count; i++)
 		{
 			printf("        %08"PRIu64": ", string_offset + (i + 1) * sizeof(QWORD));;	
-			impl_print_lstring(chunk->code + *((uint64_t*)(chunk->code + string_offset) + i + 1));
+			print_lstring(chunk->code + *((uint64_t*)(chunk->code + string_offset) + i + 1));
 			fputc('\n', stdout);
 		}
 	}
@@ -95,13 +77,13 @@ void ChunkDisassemble(const Chunk* chunk, const char* name)
 		//loop to print byte-code to execute
 		for (uint64_t offset = code_offset; offset < chunk->count;)
 		{
-			offset = impl_chunk_print_code(chunk, offset);
+			offset = dis_chunk_print_code(chunk, offset);
 		}
 	}
 
 }
 
-void impl_print_lstring(const char* str)
+void print_lstring(const char* str)
 {
 	fputs(CONSOLE_FOREGROUND_YELLOW"\"", stdout);
 	//TODO: we need to add size information for string
@@ -137,7 +119,7 @@ void impl_print_lstring(const char* str)
 	fputs("\""CONSOLE_COLOR_RESET, stdout);
 }
 
-void impl_print_global_variable(const Chunk* chunk, uint64_t var_nb)
+void unsafe_print_global_variable(const Chunk* chunk, uint64_t var_nb)
 {
 	uint64_t debug_offset = ChunkGetDEBUGSection(chunk);
 	uint64_t offset = ChunkGetGLOBALSection(chunk);
@@ -149,14 +131,14 @@ void impl_print_global_variable(const Chunk* chunk, uint64_t var_nb)
 		
 	QWORD value = *(QWORD*)(chunk->code + offset + var_nb * 8);
 	if (id == ID_COLT_LSTRING)
-		impl_print_lstring(chunk->code + *(uint64_t*)(chunk->code + ChunkGetSTRINGSection(chunk) + (value.u64 + 1) * sizeof(QWORD)));
+		print_lstring(chunk->code + *(uint64_t*)(chunk->code + ChunkGetSTRINGSection(chunk) + (value.u64 + 1) * sizeof(QWORD)));
 	else
 		OpCode_Print(value, id);
 
 	fputs(CONSOLE_COLOR_RESET ";\n", stdout);
 }
 
-uint64_t impl_chunk_print_code(const Chunk* chunk, uint64_t offset)
+uint64_t dis_chunk_print_code(const Chunk* chunk, uint64_t offset)
 {
 	printf("        %08"PRIu64" ", offset);
 
@@ -166,120 +148,120 @@ uint64_t impl_chunk_print_code(const Chunk* chunk, uint64_t offset)
 		/******************************************************/
 
 	case OP_LOAD_GLOBAL:
-		impl_print_global_instruction("OP_LOAD_GLOBAL", ChunkGetQWORD(chunk, &offset).u64, chunk);		
+		dis_print_global_instruction("OP_LOAD_GLOBAL", ChunkGetQWORD(chunk, &offset).u64, chunk);		
 		return offset;
 
 	case OP_STORE_GLOBAL:
-		impl_print_global_instruction("OP_STORE_GLOBAL", ChunkGetQWORD(chunk, &offset).u64, chunk);
+		dis_print_global_instruction("OP_STORE_GLOBAL", ChunkGetQWORD(chunk, &offset).u64, chunk);
 		return offset;
 
 	case OP_LOAD_LSTRING:
-		return impl_print_simple_instruction("OP_LOAD_LSTRING", offset);
+		return dis_print_simple_instruction("OP_LOAD_LSTRING", offset);
 
 	case OP_STORE_LSTRING:
-		return impl_print_simple_instruction("OP_STORE_LSTRING", offset);
+		return dis_print_simple_instruction("OP_STORE_LSTRING", offset);
 
 	case OP_PUSH_BYTE:
-		impl_print_hex_instruction("OP_PUSH_BYTE", ChunkGetBYTE(chunk, &offset).u8);
+		dis_print_hex_instruction("OP_PUSH_BYTE", ChunkGetBYTE(chunk, &offset).u8);
 		return offset;
 
 	case OP_PUSH_WORD:
-		impl_print_hex_instruction("OP_PUSH_WORD", ChunkGetWORD(chunk, &offset).u16);
+		dis_print_hex_instruction("OP_PUSH_WORD", ChunkGetWORD(chunk, &offset).u16);
 		return offset;
 
 	case OP_PUSH_DWORD:
-		impl_print_hex_instruction("OP_PUSH_DWORD", ChunkGetDWORD(chunk, &offset).u32);
+		dis_print_hex_instruction("OP_PUSH_DWORD", ChunkGetDWORD(chunk, &offset).u32);
 		return offset;
 
 	case OP_PUSH_QWORD:
-		impl_print_hex_instruction("OP_PUSH_QWORD", ChunkGetQWORD(chunk, &offset).u64);
+		dis_print_hex_instruction("OP_PUSH_QWORD", ChunkGetQWORD(chunk, &offset).u64);
 		return offset;
 
 	case OP_PUSH_SCOPE:
-		impl_print_hex_instruction("OP_PUSH_SCOPE", ChunkGetDWORD(chunk, &offset).u32);
+		dis_print_hex_instruction("OP_PUSH_SCOPE", ChunkGetDWORD(chunk, &offset).u32);
 		return offset;
 	
 	case OP_POP_SCOPE:
-		impl_print_hex_instruction("OP_POP_SCOPE", ChunkGetDWORD(chunk, &offset).u32);
+		dis_print_hex_instruction("OP_POP_SCOPE", ChunkGetDWORD(chunk, &offset).u32);
 		return offset;
 
 	case OP_SLOAD_LOCAL:
-		impl_print_hex_instruction("OP_SLOAD_LOCAL", ChunkGetBYTE(chunk, &offset).u8);
+		dis_print_hex_instruction("OP_SLOAD_LOCAL", ChunkGetBYTE(chunk, &offset).u8);
 		return offset;
 
 	case OP_SSTORE_LOCAL:
-		impl_print_hex_instruction("OP_SSTORE_LOCAL", ChunkGetBYTE(chunk, &offset).u8);
+		dis_print_hex_instruction("OP_SSTORE_LOCAL", ChunkGetBYTE(chunk, &offset).u8);
 		return offset;
 
 	case OP_LOAD_LOCAL:
-		impl_print_hex_instruction("OP_LOAD_LOCAL", ChunkGetDWORD(chunk, &offset).u32);
+		dis_print_hex_instruction("OP_LOAD_LOCAL", ChunkGetDWORD(chunk, &offset).u32);
 		return offset;
 
 	case OP_STORE_LOCAL:
-		impl_print_hex_instruction("OP_STORE_LOCAL", ChunkGetDWORD(chunk, &offset).u32);
+		dis_print_hex_instruction("OP_STORE_LOCAL", ChunkGetDWORD(chunk, &offset).u32);
 		return offset;
 
 		/******************************************************/
 
 	case OP_CONVERT:
-		return impl_print_2operand_instruction("OP_CONVERT", chunk->code[offset + 1], chunk->code[offset + 2], offset);
+		return dis_print_2operand_instruction("OP_CONVERT", chunk->code[offset + 1], chunk->code[offset + 2], offset);
 
 		/******************************************************/
 
 	case OP_NEGATE:
-		return impl_print_operand_instruction("OP_NEGATE", chunk->code[offset + 1], offset);
+		return dis_print_operand_instruction("OP_NEGATE", chunk->code[offset + 1], offset);
 
 	case OP_ADD:
-		return impl_print_operand_instruction("OP_ADD", chunk->code[offset + 1], offset);
+		return dis_print_operand_instruction("OP_ADD", chunk->code[offset + 1], offset);
 	case OP_SUBTRACT:
-		return impl_print_operand_instruction("OP_SUBTRACT", chunk->code[offset + 1], offset);
+		return dis_print_operand_instruction("OP_SUBTRACT", chunk->code[offset + 1], offset);
 	case OP_MULTIPLY:
-		return impl_print_operand_instruction("OP_MULTIPLY", chunk->code[offset + 1], offset);
+		return dis_print_operand_instruction("OP_MULTIPLY", chunk->code[offset + 1], offset);
 	case OP_DIVIDE:
-		return impl_print_operand_instruction("OP_DIVIDE", chunk->code[offset + 1], offset);
+		return dis_print_operand_instruction("OP_DIVIDE", chunk->code[offset + 1], offset);
 	case OP_MODULO:
-		return impl_print_operand_instruction("OP_MODULO", chunk->code[offset + 1], offset);
+		return dis_print_operand_instruction("OP_MODULO", chunk->code[offset + 1], offset);
 	case OP_BIT_AND:
-		return impl_print_operand_instruction("OP_BIT_AND", chunk->code[offset + 1], offset);
+		return dis_print_operand_instruction("OP_BIT_AND", chunk->code[offset + 1], offset);
 	case OP_BIT_OR:
-		return impl_print_operand_instruction("OP_BIT_OR", chunk->code[offset + 1], offset);
+		return dis_print_operand_instruction("OP_BIT_OR", chunk->code[offset + 1], offset);
 	case OP_BIT_XOR:
-		return impl_print_operand_instruction("OP_BIT_XOR", chunk->code[offset + 1], offset);
+		return dis_print_operand_instruction("OP_BIT_XOR", chunk->code[offset + 1], offset);
 	case OP_BIT_NOT:
-		return impl_print_operand_instruction("OP_BIT_NOT", chunk->code[offset + 1], offset);
+		return dis_print_operand_instruction("OP_BIT_NOT", chunk->code[offset + 1], offset);
 	case OP_BIT_SHIFT_L:
-		return impl_print_operand_instruction("OP_BIT_SHIFT_L", chunk->code[offset + 1], offset);
+		return dis_print_operand_instruction("OP_BIT_SHIFT_L", chunk->code[offset + 1], offset);
 	case OP_BIT_SHIFT_R:
-		return impl_print_operand_instruction("OP_BIT_SHIFT_R", chunk->code[offset + 1], offset);
+		return dis_print_operand_instruction("OP_BIT_SHIFT_R", chunk->code[offset + 1], offset);
 	case OP_BOOL_NOT:
-		return impl_print_operand_instruction("OP_BOOL_NOT", chunk->code[offset + 1], offset);
+		return dis_print_operand_instruction("OP_BOOL_NOT", chunk->code[offset + 1], offset);
 	case OP_CMP_GREATER:
-		return impl_print_operand_instruction("OP_CMP_GREATER", chunk->code[offset + 1], offset);
+		return dis_print_operand_instruction("OP_CMP_GREATER", chunk->code[offset + 1], offset);
 	case OP_CMP_GREATER_EQ:
-		return impl_print_operand_instruction("OP_CMP_GREATER_EQ", chunk->code[offset + 1], offset);
+		return dis_print_operand_instruction("OP_CMP_GREATER_EQ", chunk->code[offset + 1], offset);
 	case OP_CMP_LESS:
-		return impl_print_operand_instruction("OP_CMP_LESS", chunk->code[offset + 1], offset);
+		return dis_print_operand_instruction("OP_CMP_LESS", chunk->code[offset + 1], offset);
 	case OP_CMP_LESS_EQ:
-		return impl_print_operand_instruction("OP_CMP_LESS_EQ", chunk->code[offset + 1], offset);
+		return dis_print_operand_instruction("OP_CMP_LESS_EQ", chunk->code[offset + 1], offset);
 	case OP_CMP_EQUAL:
-		return impl_print_operand_instruction("OP_CMP_EQUAL", chunk->code[offset + 1], offset);
+		return dis_print_operand_instruction("OP_CMP_EQUAL", chunk->code[offset + 1], offset);
 	case OP_CMP_NOT_EQUAL:
-		return impl_print_operand_instruction("OP_CMP_NOT_EQUAL", chunk->code[offset + 1], offset);
+		return dis_print_operand_instruction("OP_CMP_NOT_EQUAL", chunk->code[offset + 1], offset);
 
 		/******************************************************/
 
 	case OP_SJUMP_GREATER:
-		return impl_print_sjump_instruction("OP_SJUMP_GREATER", chunk->code[offset + 1], chunk->code[offset + 2], offset);
+		return dis_print_sjump_instruction("OP_SJUMP_GREATER", chunk->code[offset + 1], chunk->code[offset + 2], offset);
 	case OP_SJUMP_GREATER_EQ:
-		return impl_print_sjump_instruction("OP_SJUMP_GREATER_EQ", chunk->code[offset + 1], chunk->code[offset + 2], offset);
+		return dis_print_sjump_instruction("OP_SJUMP_GREATER_EQ", chunk->code[offset + 1], chunk->code[offset + 2], offset);
 	case OP_SJUMP_LESS:
-		return impl_print_sjump_instruction("OP_SJUMP_LESS", chunk->code[offset + 1], chunk->code[offset + 2], offset);
+		return dis_print_sjump_instruction("OP_SJUMP_LESS", chunk->code[offset + 1], chunk->code[offset + 2], offset);
 	case OP_SJUMP_LESS_EQ:
-		return impl_print_sjump_instruction("OP_SJUMP_LESS_EQ", chunk->code[offset + 1], chunk->code[offset + 2], offset);
+		return dis_print_sjump_instruction("OP_SJUMP_LESS_EQ", chunk->code[offset + 1], chunk->code[offset + 2], offset);
 	case OP_SJUMP_EQUAL:
-		return impl_print_sjump_instruction("OP_SJUMP_EQUAL", chunk->code[offset + 1], chunk->code[offset + 2], offset);
+		return dis_print_sjump_instruction("OP_SJUMP_EQUAL", chunk->code[offset + 1], chunk->code[offset + 2], offset);
 	case OP_SJUMP_NOT_EQUAL:
-		return impl_print_sjump_instruction("OP_SJUMP_NOT_EQUAL", chunk->code[offset + 1], chunk->code[offset + 2], offset);
+		return dis_print_sjump_instruction("OP_SJUMP_NOT_EQUAL", chunk->code[offset + 1], chunk->code[offset + 2], offset);
 	case OP_SJUMP_TRUE:
 		printf(CONSOLE_FOREGROUND_YELLOW "%-20s "
 			CONSOLE_FOREGROUND_CYAN "TO: '%"PRIu64"'\n"
@@ -294,17 +276,17 @@ uint64_t impl_chunk_print_code(const Chunk* chunk, uint64_t offset)
 		/******************************************************/
 
 	case OP_PRINT:
-		return impl_print_operand_instruction("OP_PRINT", chunk->code[offset + 1], offset);
+		return dis_print_operand_instruction("OP_PRINT", chunk->code[offset + 1], offset);
 
 		/******************************************************/
 
 	case OP_RETURN:
-		return impl_print_simple_instruction("OP_RETURN", offset);
+		return dis_print_simple_instruction("OP_RETURN", offset);
 	case OP_POP:
-		return impl_print_simple_instruction("OP_POP", offset);
+		return dis_print_simple_instruction("OP_POP", offset);
 
 	case OP_EXIT:
-		impl_print_hex_instruction("OP_EXIT", ChunkGetQWORD(chunk, &offset).u64);
+		dis_print_hex_instruction("OP_EXIT", ChunkGetQWORD(chunk, &offset).u64);
 		return offset;
 
 	default:
@@ -314,14 +296,14 @@ uint64_t impl_chunk_print_code(const Chunk* chunk, uint64_t offset)
 	}
 }
 
-uint64_t impl_print_simple_instruction(const char* name, uint64_t offset)
+uint64_t dis_print_simple_instruction(const char* name, uint64_t offset)
 {
 	printf(CONSOLE_FOREGROUND_GREEN "%-20s\n"
 		CONSOLE_COLOR_RESET, name);
 	return offset + 1;
 }
 
-uint64_t impl_print_operand_instruction(const char* name, uint8_t byte, uint64_t offset)
+uint64_t dis_print_operand_instruction(const char* name, uint8_t byte, uint64_t offset)
 {
 	printf(CONSOLE_FOREGROUND_BRIGHT_MAGENTA "%-20s "
 		CONSOLE_FOREGROUND_CYAN "'%s'\n"
@@ -329,7 +311,7 @@ uint64_t impl_print_operand_instruction(const char* name, uint8_t byte, uint64_t
 	return offset + 2;
 }
 
-uint64_t impl_print_2operand_instruction(const char* name, uint8_t first, uint8_t second, uint64_t offset)
+uint64_t dis_print_2operand_instruction(const char* name, uint8_t first, uint8_t second, uint64_t offset)
 {
 	printf(CONSOLE_FOREGROUND_YELLOW "%-20s "
 		CONSOLE_FOREGROUND_CYAN "'%s'" CONSOLE_COLOR_RESET ", "
@@ -338,7 +320,7 @@ uint64_t impl_print_2operand_instruction(const char* name, uint8_t first, uint8_
 	return offset + 3;
 }
 
-uint64_t impl_print_sjump_instruction(const char* name, uint8_t type, uint8_t to_offset, uint64_t offset)
+uint64_t dis_print_sjump_instruction(const char* name, uint8_t type, uint8_t to_offset, uint64_t offset)
 {
 	printf(CONSOLE_FOREGROUND_YELLOW "%-20s "
 		CONSOLE_FOREGROUND_CYAN "'%s'" CONSOLE_COLOR_RESET ", "
@@ -347,14 +329,14 @@ uint64_t impl_print_sjump_instruction(const char* name, uint8_t type, uint8_t to
 	return offset + 3;
 }
 
-void impl_print_hex_instruction(const char* name, uint64_t value)
+void dis_print_hex_instruction(const char* name, uint64_t value)
 {
 	printf(CONSOLE_FOREGROUND_BRIGHT_BLUE "%-20s "
 		CONSOLE_FOREGROUND_BRIGHT_GREEN "'0x%"PRIX64"'\n"
 		CONSOLE_COLOR_RESET, name, value);
 }
 
-void impl_print_global_instruction(const char* name, uint64_t byte_offset, const Chunk* chunk)
+void dis_print_global_instruction(const char* name, uint64_t byte_offset, const Chunk* chunk)
 {
 	uint64_t debug_offset = ChunkGetDEBUGSection(chunk);
 	uint64_t var_nb = (byte_offset - ChunkGetGLOBALSection(chunk)) / sizeof(QWORD);
--- colt/src/byte-code/disassemble.h
+++ colt/src/byte-code/disassemble.h
@@ -19,31 +19,31 @@ IMPLEMENTATION HELPERS
 
 /// @brief Writes a string to 'stdout', highlighting escape characters, and quoting it
 /// @param str The string to print
-void impl_print_lstring(const char* str);
+void print_lstring(const char* str);
 
 /// @brief Writes the value of a global value and its type
 /// @param chunk The Chunk from which to read
 /// @param var_nb The current global variable counter
-void impl_print_global_variable(const Chunk* chunk, uint64_t var_nb);
+void unsafe_print_global_variable(const Chunk* chunk, uint64_t var_nb);
 
 /// @brief Dispatches a code to the correct printing function
 /// @param chunk The chunk from which to extract the code
 /// @param offset The offset to the code
 /// @return Modified offset
-uint64_t impl_chunk_print_code(const Chunk* chunk, uint64_t offset);
+uint64_t dis_chunk_print_code(const Chunk* chunk, uint64_t offset);
 
 /// @brief Prints a one byte instruction
 /// @param name The name of the instruction
 /// @param offset The current byte offset
 /// @return The current byte offset + 1
-uint64_t impl_print_simple_instruction(const char* name, uint64_t offset);
+uint64_t dis_print_simple_instruction(const char* name, uint64_t offset);
 
 /// @brief Prints a one byte instruction followed by the operand following it
 /// @param name The name of the instruction
 /// @param byte The byte that follows it
 /// @param offset The current byte offset
 /// @return The current byte offset + 2
-uint64_t impl_print_operand_instruction(const char* name, uint8_t byte, uint64_t offset);
+uint64_t dis_print_operand_instruction(const char* name, uint8_t byte, uint64_t offset);
 
 /// @brief Prints a one byte instruction followed by the 2 operands following it
 /// @param name The name of the instruction
@@ -51,7 +51,7 @@ uint64_t impl_print_operand_instruction(const char* name, uint8_t byte, uint64_t
 /// @param second The second operand
 /// @param offset The current byte offset
 /// @return The current byte offset + 3
-uint64_t impl_print_2operand_instruction(const char* name, uint8_t first, uint8_t second, uint64_t offset);
+uint64_t dis_print_2operand_instruction(const char* name, uint8_t first, uint8_t second, uint64_t offset);
 
 /// @brief Prints a short jump instruction, which is followed by a type operand, and a byte offset
 /// @param name The name of the instruction
@@ -59,19 +59,19 @@ uint64_t impl_print_2operand_instruction(const char* name, uint8_t first, uint8_
 /// @param to_offset The offset value
 /// @param offset The current byte offset
 /// @return The current byte offset + 3
-uint64_t impl_print_sjump_instruction(const char* name, uint8_t type, uint8_t to_offset, uint64_t offset);
+uint64_t dis_print_sjump_instruction(const char* name, uint8_t type, uint8_t to_offset, uint64_t offset);
 
 /// @brief Prints a one byte instruction followed by the int following it.
 /// There is no offset to pass to this function, but rather, the 'value' argument
 /// should be ChunkGetUInt[16|32|64](..., &offset).
 /// @param name The name of the instruction
 /// @param value The int following the instruction
-void impl_print_hex_instruction(const char* name, uint64_t value);
+void dis_print_hex_instruction(const char* name, uint64_t value);
 
 /// @brief Prints a global variable name for read/write OpCodes
 /// @param name The name of the instruction
 /// @param byte_offset The offset of the QWORD (from ChunkGetQWORD)
 /// @param chunk The Chunk from which to read debug data
-void impl_print_global_instruction(const char* name, uint64_t byte_offset, const Chunk* chunk);
+void dis_print_global_instruction(const char* name, uint64_t byte_offset, const Chunk* chunk);
 
 #endif //HG_COLT_DISASSEMBLE
