52fba4276b403078b0a6484f462c8f54d7e8c1ed
R533-Code <raphaeldibnehmepc@gmail.com>
2022-07-13 19:11:31 +0300

    Fixed invariants of OP_JUMP[_NOT]_TRUE

--- colt/src/byte-code/byte_code.h
+++ colt/src/byte-code/byte_code.h
@@ -201,20 +201,20 @@ typedef enum
 	/// The JUMP_OFFSET is an unsigned byte which is added to the instruction pointer of the VM.
 	OP_SJUMP_NOT_EQUAL,
 
-	/// @brief Short Jump if True, if the top QWORD.b == 1, perform a jump
+	/// @brief Short Jump if True, pops the top, if the top QWORD.b == 1, perform a jump
 	/// [OP_SJUMP_TRUE][JUMP_OFFSET]
 	/// The JUMP_OFFSET is an unsigned byte which is added to the instruction pointer of the VM.
 	OP_SJUMP_TRUE,
-	/// @brief Short Jump if Not True, if the top QWORD.b == 0, if it evaluates to a true, perform a jump
+	/// @brief Short Jump if Not True, pops the top, if the top QWORD.b == 0, perform a jump
 	/// [OP_SJUMP_NOT_TRUE][JUMP_OFFSET]
 	/// The JUMP_OFFSET is an unsigned byte which is added to the instruction pointer of the VM.
 	OP_SJUMP_NOT_TRUE,
 	
-	/// @brief Jump if True, if the top QWORD.b == 1, if it evaluates to a true, perform a jump
+	/// @brief Jump if True, pops the top, if the top QWORD.b == 1, if it evaluates to a true, perform a jump
 	/// [OP_SJUMP_NOT_TRUE][PADDING]?[JUMP_OFFSET]
 	/// The JUMP_OFFSET is an aligned uint32_t assigned to the instruction pointer of the VM.
 	OP_JUMP_TRUE,
-	/// @brief Jump if Not True, if the top QWORD.b == 0, if it evaluates to a true, perform a jump
+	/// @brief Jump if Not True, pops the top if the top QWORD.b == 0, if it evaluates to a true, perform a jump
 	/// [OP_SJUMP_NOT_TRUE][PADDING]?[JUMP_OFFSET]
 	/// The JUMP_OFFSET is an aligned uint32_t assigned to the instruction pointer of the VM.
 	OP_JUMP_NOT_TRUE,
