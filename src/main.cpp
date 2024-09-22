263031a7909aaa0e02371fdc48897d3d80eed3a8
R533-Code <raphaeldibnehmepc@gmail.com>
2024-09-22 18:47:14 +0200

    Made use of new file to parse command line arguments

--- src/main.cpp
+++ src/main.cpp
@@ -1,49 +1,10 @@
 #include <colt_pch.h>
-#include <colt_config.h>
-#include <fmt/ranges.h>
-#include <colt/os/dynamic_lib.h>
+#include <util/args.h>
 
-struct ColoredVersion
-{
-  clt::io::ANSIEffect color;
-  const char* name;
-  clt::vers::Version version;
-};
-
-template<>
-struct fmt::formatter<ColoredVersion>
-{
-  template<typename ParseContext>
-  constexpr auto parse(ParseContext& ctx)
-  {
-    // TODO: add format option
-    return ctx.begin();
-  }
-
-  template<typename FormatContext>
-  auto format(const ColoredVersion& version, FormatContext& ctx) const
-  {
-    return fmt::format_to(
-        ctx.out(), "{}{:<8}{} : v{}", version.color, version.name, clt::io::Reset, version.version);
-  }
-};
+using namespace clt;
 
 int main(int argc, const char** argv)
 {
-  using namespace clt;
-  using namespace clt::io;
-  
-  static std::array Versions = {
-      ColoredVersion{BrightGreenF, "coltcpp", vers::ColtCppVersion},
-      ColoredVersion{BrightYellowF, "fmt", vers::FmtVersion},
-      ColoredVersion{BrightYellowF, "scn", vers::ScnVersion},
-      ColoredVersion{BrightYellowF, "simdutf", vers::SimdUtfVersion},
-      ColoredVersion{BrightYellowF, "unialgo", vers::UniAlgoVersion},
-      ColoredVersion{BrightYellowF, "unicode", vers::UnicodeVersion},
-  };
-
-
-  io::print("{}coltc{} (v{} {}) on {}{}{} ({}{}{})\nusing: {}",
-      BrightCyanF, Reset, vers::ColtcVersion, COLT_CONFIG_STRING, BrightBlueF, COLT_OS_STRING, Reset,
-      BrightMagentaF, COLT_ARCH_STRING, Reset, fmt::join(Versions, "\n       "));
+  cl::parse_command_line_options<CMDs>(
+      argc, argv, COLTC_EXECUTABLE_NAME, "The Colt compiler.");
 }
