6b3ca87863bb6af1004afd3e6e09814a90eb77ba
R533-Code <raphaeldibnehmepc@gmail.com>
2024-09-23 22:05:53 +0200

    Replaced main with colt_main

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,16 @@
 
 using namespace clt;
 
-int main(int argc, const char** argv)
+/// @brief This is the main entry point of the compiler.
+/// The true main function is defined by colt-cpp, which
+/// converts command line arguments to UTF8 and sets up
+/// console support for Unicode on Windows.
+/// @param argv The arguments value
+/// @return The exit code
+int colt_main(Span<const char8_t*> argv)
 {
   cl::parse_command_line_options<CMDs>(
-      argc, argv, COLTC_EXECUTABLE_NAME, "The Colt compiler.");
+      argv, COLTC_EXECUTABLE_NAME, "The Colt compiler.");
   io::print_warn("REPL is not implemented.");
+  return 0;
 }
