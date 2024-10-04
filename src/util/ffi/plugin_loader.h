f2f91db93ede20c0c13a3e2afdbfe83044d06d69
R533-Code <raphaeldibnehmepc@gmail.com>
2024-10-04 15:36:02 +0200

    Improved API and error handling

--- src/util/ffi/plugin_loader.h
+++ src/util/ffi/plugin_loader.h
@@ -30,21 +30,23 @@ namespace clt::ffi
     {
       /// @brief The path is either invalid, or the library could not be opened
       OS_ERR,
-      /// @brief colt_setup returned a non-zero value.
+      /// @brief 'colt_name' and 'colt_purpose' where not found in the library
+      MISSING_FN,
+      /// @brief 'colt_setup' returned a non-zero value.
       SETUP_ERR,
     };
 
     /// @brief The advertised features of the plugin
     enum class PluginType : u8
     {
-      /// @brief Plugin that only provides a name
-      EMPTY,
       /// @brief Plugin to which to transfer compiler generated errors
       REPORTER,
       /// @brief Plugin to which to pass a string to highlight
       HIGHLIGHT,
       /// @brief Plugin to which to pass the resulting AST
       BACKEND,
+      /// @brief Plugin that only provides a name
+      EMPTY,
     };
 
     /// @brief Function that returns a string, without taking any arguments.
@@ -64,6 +66,24 @@ namespace clt::ffi
       return lib.find<fn_name_t>("colt_name")
           .and_then([](fn_name_t a) { return Option<u8ZStringView>(a()); })
           .value_or(u8ZStringView{});
+    }
+
+    /// @brief The advertised purpose of the plugin.
+    /// The purpose is 'advertised' as the plugin can be lacking
+    /// the necessary functions to fulfill its purpose.
+    /// @return PluginType
+    PluginType advertised_purpose() const
+    {
+      return lib.find<fn_type_t>("colt_purpose")
+          .and_then(
+              [](fn_type_t a)
+              {
+                auto val = a();
+                if (val > PluginType::EMPTY)
+                  return Option<PluginType>(None);
+                return Option<PluginType>(val);
+              })
+          .value_or(PluginType::EMPTY);
     }
 
     /// @brief Returns the description of the plugin if it exist
@@ -115,7 +135,10 @@ namespace clt::ffi
         const char* path, bool manual_setup = false) noexcept
     {
       auto lib = os::DynamicLib::open(path);
-      if (lib == None || lib->find_symbol("colt_name") == nullptr)
+      if (lib == None)
+        return {Error, OpenError::OS_ERR};
+      if (lib->find_symbol("colt_name") == nullptr
+          && lib->find_symbol("colt_purpose") == nullptr)
         return {Error, OpenError::OS_ERR};
       auto plugin = ColtPlugin(std::move(*lib));
       if (manual_setup)
@@ -136,5 +159,8 @@ namespace clt::ffi
     }
   };
 } // namespace clt::ffi
+
+ADD_REFLECTION_FOR_CONSECUTIVE_ENUM(
+    clt::ffi::ColtPlugin, PluginType, REPORTER, HIGHLIGHT, BACKEND, EMPTY);
 
 #endif // !HG_COLTC_PLUGIN_LOADER
