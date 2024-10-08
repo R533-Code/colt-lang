9783376648a525057e462f9d7569fae49366a020
R533-Code <raphaeldibnehmepc@gmail.com>
2024-10-08 17:10:09 +0200

    Added "COMPILER LIMIT: " string for compiler_assert_true

--- src/frontend/err/compiler_limits.h
+++ src/frontend/err/compiler_limits.h
@@ -1,4 +1,4 @@
-/*****************************************************************//**
+/*****************************************************************/ /**
  * @file   compiler_limits.h
  * @brief  Contains assertion macros used in the compiler.
  * These macros are not deactivated on Release: they are usually
@@ -14,11 +14,12 @@
 
 #ifndef COLT_NO_COMPILER_ASSERTS
   /// @brief Assertion that is always checked
-  #define compiler_assert_true(MESSAGE, COND, ...)                                  \
-    clt::details::assert_true_multiple(                                             \
-        MESSAGE, clt::source_location::current() __DETAILS__COLT_TO_ASSERTION(COND) \
-                     COLT_FOR_EACH(__DETAILS__COLT_TO_ASSERTION, __VA_ARGS__))      \
-        __DETAILS__COLT_TO_ASSUME(COND)                                             \
+  #define compiler_assert_true(MESSAGE, COND, ...)                         \
+    clt::details::assert_true_multiple(                                    \
+        "COMPILER LIMIT: " MESSAGE,                                        \
+        clt::source_location::current() __DETAILS__COLT_TO_ASSERTION(COND) \
+            COLT_FOR_EACH(__DETAILS__COLT_TO_ASSERTION, __VA_ARGS__))      \
+        __DETAILS__COLT_TO_ASSUME(COND)                                    \
             COLT_FOR_EACH(__DETAILS__COLT_TO_ASSUME, __VA_ARGS__)
 #else
   #define compiler_assert_true(MESSAGE, COND, ...) ((void)0)
