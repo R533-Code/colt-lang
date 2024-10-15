f70b646a477a182fe9309bbe9cb380bee567ecb3
R533-Code <raphaeldibnehmepc@gmail.com>
2024-10-15 13:57:20 +0200

    Added TargetInfo

---
+++ src/frontend/target_info.h
@@ -0,0 +1,59 @@
+/*****************************************************************/ /**
+ * @file   target_info.h
+ * @brief  Contains TargetInfo, which describes basic informations
+ *         about the target to compile to.
+ * 
+ * @author RPC
+ * @date   October 2024
+ *********************************************************************/
+#ifndef HG_COLT_TARGET_INFO
+#define HG_COLT_TARGET_INFO
+
+#include <colt/bit/endian.h>
+#include <colt/meta/reflect.h>
+
+DECLARE_ENUM_WITH_TYPE(
+    u8, clt::lng, PrimitiveType, iw8b, iw16b, iw32b, iw64b, fw32b, fw64b);
+
+namespace clt::lng
+{
+  /// @brief Describes a target informations low-level informations.
+  /// Describes the alignment and size informations of primitive types.
+  /// For now, there is no need to add struct support as the primitive
+  /// types might be enough to compute alignment and size
+  struct TargetInfo
+  {
+    /// @brief The alignment and size information of primitive types.
+    struct PrimitiveTypeInfo
+    {
+      /// @brief The size in bytes
+      u8 size;
+      /// @brief The alignment in bytes
+      /// This is usually equal to the size.
+      u8 align;
+    };
+
+    /// @brief The endianness of the target
+    enum class Endianess : u8
+    {
+      /// @brief Little Endian
+      little_endian,
+      /// @brief Big Endian
+      big_endian,
+    };
+
+    /// @brief The endianness of the target
+    Endianess endian;
+    /// @brief The size of data pointers in bytes
+    u8 data_ptr_size;
+    /// @brief The size of function pointers in bytes
+    /// This is usually equal to 'data_ptr_size'.
+    u8 func_ptr_size;
+
+    /// @brief The information that describe primitive types.
+    std::array<PrimitiveTypeInfo, meta::reflect<PrimitiveType>::max()>
+        primitive_infos;
+  };
+} // namespace clt::lng
+
+#endif // !HG_COLT_TARGET_INFO
