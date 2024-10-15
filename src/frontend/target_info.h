3d23f6830b67a72f5a2dacc58ff174149db6a35f
R533-Code <raphaeldibnehmepc@gmail.com>
2024-10-15 14:26:14 +0200

    Added comments and fixed implementation

--- src/frontend/target_info.h
+++ src/frontend/target_info.h
@@ -11,6 +11,7 @@
 
 #include <colt/bit/endian.h>
 #include <colt/meta/reflect.h>
+#include <colt/num/math.h>
 
 DECLARE_ENUM_WITH_TYPE(
     u8, clt::lng, PrimitiveType,
@@ -55,8 +56,32 @@ namespace clt::lng
     Endianess endian;
 
     /// @brief The information that describe primitive types.
-    std::array<PrimitiveTypeInfo, meta::reflect<PrimitiveType>::max()>
+    /// This array is indexed by PrimitiveType.
+    std::array<PrimitiveTypeInfo, meta::reflect<PrimitiveType>::count()>
         primitive_infos;
+
+    /// @brief The type info of void* on the targeted platform
+    /// As void* must be able to store either data or function pointers,
+    /// the size and alignment is the maximum of both informations
+    /// @return Information about void* on the targeted platform
+    constexpr PrimitiveTypeInfo void_ptr_info() const noexcept
+    {
+      using enum PrimitiveType;
+
+      return PrimitiveTypeInfo{
+          math::max(
+              primitive_infos[(u8)dtptr].size, primitive_infos[(u8)fnptr].size),
+          math::max(
+              primitive_infos[(u8)dtptr].align, primitive_infos[(u8)fnptr].align)};
+    }
+
+    /// @brief The type info of size_t on the targeted platform.
+    /// This is the same as void_ptr_info().
+    /// @return Information about size_t on the targeted platform
+    constexpr PrimitiveTypeInfo size_t_info() const noexcept
+    {
+      return void_ptr_info();
+    }
   };
 } // namespace clt::lng
 
